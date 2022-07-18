67781a4747d7fab53bf08a0f25dec893a0828b96
daviskyLV <ledaviskylv@gmail.com>
2022-07-18 10:51:11 +0200

    Inventory & ItemType implementation

---
+++ src/main/java/app/models/Inventory.java
@@ -0,0 +1,56 @@
+package app.models;
+
+import app.utils.ItemType;
+
+import java.util.ArrayList;
+
+public class Inventory implements Main, AddItem {
+    private ArrayList<Item> items;
+
+    public Inventory() {
+        //TODO: add modelfactory
+        items = new ArrayList<>(load());
+    }
+
+    @Override
+    public ArrayList<Item> getAllItems() {
+        return new ArrayList<>(items);
+    }
+
+    @Override
+    public void removeItem(Item item) {
+        items.remove(item);
+        save();
+    }
+
+    @Override
+    public void addItem(Item item) {
+        items.add(item);
+        save();
+    }
+
+    @Override
+    public ArrayList<Item> searchItems(String query, ItemType filter) {
+        ArrayList<Item> matching = new ArrayList<>();
+        for (Item item: items) {
+            // checking does type match filter or filter is set null (aka all) and title contains query
+            if ((item.getType() == filter || filter == null) &&
+                (query.length() <= 0 || item.getTitle().toLowerCase().contains(query))) {
+                matching.add(item);
+            }
+        }
+
+        return matching;
+    }
+
+    /**
+     * Saves currently loaded items to XML file
+     */
+    private void save() {
+
+    }
+
+    private ArrayList<Item> load() {
+
+    }
+}
---
+++ src/main/java/app/models/Main.java
@@ -0,0 +1,30 @@
+package app.models;
+
+import app.utils.ItemType;
+
+import java.util.ArrayList;
+
+/**
+ * Interface mainly for MainViewModel to interact with Inventory
+ */
+public interface Main {
+    /**
+     * Searches through all items in inventory and returns matching results
+     * @param query The item's title to search for
+     * @param filter Filter by specific item title, null if all items included
+     * @return List of all matching item results
+     */
+    ArrayList<Item> searchItems(String query, ItemType filter);
+
+    /**
+     * Removes an item from inventory
+     * @param item The item to remove
+     */
+    void removeItem(Item item);
+
+    /**
+     * Gets all items from the inventory
+     * @return List of all items
+     */
+    ArrayList<Item> getAllItems();
+}
