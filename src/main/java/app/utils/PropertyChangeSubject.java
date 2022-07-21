8d692bafb2cc7e0da4b737aafd8163c90dd0ecb8
HighKingofMelons <highkingomelons@gmail.com>
2022-07-21 22:03:24 +0200

    Document PropertyChangeSubject interface

--- src/main/java/app/utils/PropertyChangeSubject.java
+++ src/main/java/app/utils/PropertyChangeSubject.java
@@ -3,6 +3,16 @@ package app.utils;
 import java.beans.PropertyChangeListener;
 
 public interface PropertyChangeSubject {
-    void addPropertyChangeListener(String propertyName, PropertyChangeListener listener);
-    void removePropertyChangeListener(String propertyName, PropertyChangeListener listener);
+    /**
+     * Add a given Listener to be notified of a given property's changes
+     * @param propertyName a String of the name of the property to receive changes from
+     * @param propertyChangeListener the Object to be notified
+     */
+    void addPropertyChangeListener(String propertyName, PropertyChangeListener propertyChangeListener);
+    /**
+     * Stop a given Listener from being notified of a given property's changes
+     * @param propertyName a String of the name of the property changes are received from
+     * @param propertyChangeListener the Object to no longer be notified
+     */
+    void removePropertyChangeListener(String propertyName, PropertyChangeListener propertyChangeListener);
 }
